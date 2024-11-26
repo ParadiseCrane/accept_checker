26c21519a78e4748e6d6467f8d8f9e483e8ae3f1
dsomni <pro100pro10010@gmail.com>
2024-11-27 01:07:36 +0300

    Fix [committing files] setup_precommit

--- setup_precommit.sh
+++ setup_precommit.sh
@@ -3,7 +3,7 @@ echo "#!/bin/sh
 uv run ruff format .
 uv run ruff check . --fix --exit-non-zero-on-fix
 uv run pylint . -j 0 -E --fail-on E
-FILES=\$(git diff --diff-filter=d --name-only)
+FILES=\$(git diff --diff-filter=d --name-only --cached)
 git add \$FILES
 " > .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
